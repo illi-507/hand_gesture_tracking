42085b0a133a745420a3e5f6f947704e96c1e086
wangxingjian <wang.xingjian@cytoderm.ai>
2025-12-23 15:43:38 +0800

    feat: update deploy settings

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/hand_landmark_display/",
   plugins: [react()],
+  base: "/hand_landmark_display/",
 });
